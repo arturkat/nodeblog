f13291ef3bcdaa630d38b2ea43158618d995ee23
Arthur Katrushko <akatrushko@lyonscg.com>
2018-06-28 19:41:25 +0300

    Created and Find Posts

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const Post = require('./models/post')
+
 const app = express()
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 
-const arr = ['hi', 'privet', 'bonjure']
+// const arr = ['hi', 'privet', 'bonjure']
 
-app.get('/', (req, res) => res.render('index', {arr: arr}))
+app.get('/', (req, res) => {
+    Post.find({})
+        .then(posts => {
+            console.log(posts);
+            res.render('index', {
+                posts: posts
+            })
+        })
+})
 
 app.get('/create', (req, res) => res.render('create'))
 app.post('/create', (req, res) => {
-    // console.log(req.body)
-    arr.push(req.body.text)
+    const {title, body} = req.body;
+    
+    Post.create({
+        title: title,
+        body: body
+    })
+    .then(post => {
+        console.log(post)
+    });
+
     res.redirect('/')
 })
 
