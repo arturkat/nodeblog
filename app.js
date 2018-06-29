ce7ad1fd035f96fa6e9bdfd60210e539ac27270a
Arthur Katrushko <akatrushko@lyonscg.com>
2018-06-29 14:35:12 +0300

    Add layouts and static files

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const path = require('path')
 
 const Post = require('./models/post')
 
 const app = express()
 
+// set for ejs
 app.set('view engine', 'ejs')
+// use body-parser
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+// use for static files
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    '/javascript',
+    express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist'))
+)
+
+
 
 // const arr = ['hi', 'privet', 'bonjure']
 
 app.get('/', (req, res) => {
+    console.log(`__dirname = ${__dirname }`)
+
     Post.find({})
         .then(posts => {
             console.log(posts);
