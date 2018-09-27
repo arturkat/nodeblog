630627eabfde26ac44e8e9a597859d80c1a8ffd9
Katrushko Arthur <akatrushko@lyonscg.com>
2018-09-28 01:33:11 +0300

    add

--- config.js
+++ config.js
@@ -2,7 +2,9 @@ const dotenv = require('dotenv')
 const path = require('path')
 
 const root = path.join.bind(this, __dirname)
-dotenv.config({ path: root('.env') })
+dotenv.config({
+    path: root('.env')
+})
 
 module.exports = {
     PORT: process.env.PORT || 3000,
