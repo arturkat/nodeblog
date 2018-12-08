e14ba7d90ac8a97fabdc646a83db7b5b226cc01d
Katrushko Arthur <akatrushko@lyonscg.com>
2018-12-08 06:25:24 +0200

    Trim string

--- app.js
+++ app.js
@@ -206,7 +206,7 @@ app.get('/track', (req, res) => {
 app.post('/track', (req, res) => {
     //5b7a98a4e7179a69ea61817f
     //5b7a98b3e7179a69ea618186
-    const trackID = req.body.trackID;
+    const trackID = req.body.trackID.trim();
     Product.findOne({
             trackCode: trackID
         })
